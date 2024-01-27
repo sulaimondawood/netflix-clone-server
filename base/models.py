a5a24bc6ec84f72bd9a42a8fa9371a67f1ff6460
Dawood <sulaimondawod@gmail.com>
2024-01-27 11:34:31 +0100

    added new package

--- base/models.py
+++ base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Movie(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+  title = models.CharField(max_length=255)
+  description = models.TextField()
+  image_cover = models.ImageField(upload_to='media/')
+  image = models.ImageField(upload_to='media/')
+  views = models.IntegerField(default=0)
